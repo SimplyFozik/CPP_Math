6d1e51e64c4b435a32a380161b9775082348b231
Fozik <fozikwashere@mail.ru>
2024-04-18 23:05:10 +0500

    Math.

---
+++ time4math/time4math.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <string>
+#include "only4math.h"
+
+using namespace std;
+
+int main()
+    {
+    string dota2;
+    cout << "Welcome To C++ Calculator!" << endl << "Type Something Below" << endl;
+    getline(cin, dota2);
+    Analyze(dota2, size(dota2));    
+}
